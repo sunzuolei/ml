7864cf90e0d889f90421f078dd1734b3e53e63c8
Samuel <sunzuolei@gmail.com>
2014-03-16 07:11:36 +0800

    Initialize the repository.

---
+++ Codes/P1/p1.m
@@ -0,0 +1,11 @@
+clear all; clc; close all;
+
+load '../Data/dataSingleInput.mat';
+figure('name', 'The data visualization')
+plot(data(:, 1), data(:, 2), 'rx', 'markersize', 10, 'linewidth', 2);
+grid on;
+box on;
+set(gca, 'xgrid', 'off');
+xlabel('House size in m^2');
+ylabel('House price in $');
+print('data.png', '-dpng');
