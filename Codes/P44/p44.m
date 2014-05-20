0606dfa66afb74348944f53c0a744cca6584d9d5
Samuel <sunzuolei@gmail.com>
2014-05-21 07:53:35 +0800

    Add P44

---
+++ Codes/P44/p44.m
@@ -0,0 +1,40 @@
+clear all; close all; clc;
+
+path(path, genpath('../../Codes'));
+
+load dataChap4D
+rng(742);
+
+targDigit   = 8;
+
+%% Slipt data
+numSamTrain = 4900; 
+numSamTest  = 5000 - numSamTrain;
+n           = size(X, 2);
+iRand       = randperm(5000);
+X           = X(iRand, :);
+y           = y(iRand, :); 
+xTrain      = X(1 : numSamTrain, :);
+yTrain      = y(1 : numSamTrain, :);
+yTrain      = (yTrain == targDigit);
+xTest       = X( (numSamTrain + 1 ): end, :);
+yTest       = y( (numSamTrain + 1 ): end, :);
+yTest       = (yTest == targDigit);
+
+%% Constrain the neural network
+% structNN  = [400, 25, 10];
+structNN  = [400, 25, 1];
+% structNN  = [400, 25, 25, 40, 10];
+lambda    = 1.5;
+numIter   = 500;
+
+%% Perform neural network learning and predicting
+[decTest,  proTest] = neuralNetwork(structNN, lambda, numIter,...
+        xTrain, yTrain, xTest, yTest);
+
+%% Visualize the classification result
+numRow  = 5;
+numCol  = fix( numSamTest / numRow );
+dispDigits(numRow, numCol, xTest,...
+            decTest, false, proTest, yTest);
+        
